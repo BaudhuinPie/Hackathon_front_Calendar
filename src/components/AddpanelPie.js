cb90e29f44cda7ef7b338ce034f2e3f5df80d161
BaudhuinPie <baudhuin_pierre.pro@mailo.com>
2019-12-06 07:52:55 +0100

    refactored addPanel => AddpanelPie, and Input to easier work git status By Pi 7h52

---
+++ src/components/AddpanelPie.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import Input from './Input';
+import moment from 'moment';
+import './Addpanel.css'
+
+
+class AddpanelPie extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            remainDays: this.findNumberOfDays(),
+        }
+    }
+
+    onChange = (event, index) => {
+        // let value = event.target.value;
+        // let updatedInputs = this.updateNumbersOfDays(this.state.inputs, position, value)
+        // this.setState({
+        //     inputs: updatedInputs
+        // })
+        this.setState(
+            {
+                [event.target.name] : event.target.value
+            }
+        )
+        console.log( event.target.name, event.target.value);
+    }
+
+    // updateNumbersOfDays = (inputs, position, value) => {
+    //    let newArrayInputs = [ ...inputs ]
+    //     newArrayInputs[position] = { id: position, name : value }
+
+    //     return newArrayInputs
+    // }
+
+    //produce un number of input..
+    findNumberOfDays = () => {
+        const today = moment();
+        var eventdate = moment("2019-12-25");
+        const remainingDays = eventdate.diff(today, 'days');
+        return remainingDays ;
+    }
+
+    dipslayInputs = () => {
+        let numbOfInputs = this.state.remainDays;
+        let listOfInputs = [];
+
+        let stateListInpName = [];
+        let stateListInpValue = [];
+
+        for (let i=0 ; i < numbOfInputs ; i++) {
+            let localname = `input_${i}`
+            listOfInputs.push(<Input  value ={this.state.localname}  onChange={this.onChange} description={localname} index = {i} key={i}/>); //list of inputs
+            // stateListInpName.push(localname);
+            // stateListInpValue.push(this.state.localname);
+        }
+
+        return listOfInputs;
+
+    }
+
+    componentDidMount() {
+        this.dipslayInputs();
+    }
+
+
+    render() {
+        return (
+            <>
+            <div className="addpanel-zone">
+                <h1>Add url to videos</h1>
+                <input type="date" />
+                <p>Numbers of days till Christmas: 19</p>
+
+                 {this.dipslayInputs()}
+
+                 {/* {this.state.inputs.map((input, index) => (
+                     <>
+                    <Input onChange={this.onChange} position={index} description={input} itsValue={this.state.inputs[index].name} key={index} />
+                    </>
+                 ))} */}
+                 <button className="button-send-all">Send All videos</button>
+            </div>
+            </>
+        )
+    }
+}
+
+export default AddpanelPie;
--- src/components/backoffice/BackOffice.js
+++ src/components/backoffice/BackOffice.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './BackOffice.css';
 import Addpanel from './Addpanel';
-import ASBLPanel from './ASBLPanel'
+import ASBLPanel from './ASBLPanel';
+import AddpanelPie from '../AddpanelPie';
 
 class BackOffice extends React.Component {
     render() {
         return (
             <div class="backoffice-zone">
-                <Addpanel />
+                <AddpanelPie />
                 <ASBLPanel />
             </div>
         )
